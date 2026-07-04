28820462288f54631f9c50f3cb7279867067b016
Ashish <teri-email-id-yaha-daal@gmail.com>
2026-07-04 20:31:21 +0530

    Phase 1 Complete: Deployed 100% DRY & Crash-Proof Tic-Tac-Toe with Colored UI

--- Phase_01_Foundation/tic_tac_toe/src/main.rs
+++ Phase_01_Foundation/tic_tac_toe/src/main.rs
@@ -4,21 +4,13 @@ fn main()
 {
     let mut ttt=[["1","2","3"],["4","5","6"],["7","8","9"]];
     let mut choice=String::new();
-    let mut step=1;
+    let mut step=0;
     let mut current_player="X";
     loop
     {
-        println!("\n\n");
-         for i in 0..3
-    {
-        println!(" {} | {} | {} ",ttt[i][0],ttt[i][1],ttt[i][2]);
-        if i<2
-        {
-            println!("---|---|---");
-        }
-    }
-        println!("\n\n");
-        println!("Player {} enter your task[1-9]:",current_player);
+        print_tab(&ttt);
+        println!("{}",format!("Player {} enter the position[1-9]",current_player).red().bold());
+        
         choice.clear();
         io::stdin().read_line(&mut choice).unwrap();
         let choice:usize=match choice.trim().parse::<usize>()
@@ -34,7 +26,7 @@ fn main()
         let col=(choice-1)%3;
         if ttt[row][col]=="X"||ttt[row][col]=="O"
         {
-            println!("\nThis is already filled");
+            println!("{}",format!("Cell is already filled").red().bold());
             continue;
         }
         else
@@ -60,33 +52,78 @@ fn main()
         { print_pro(&ttt,current_player,(0,2),(1,1),(2,0)); break;}
          if step==9
         {
-            println!("Game is Draw no one Wins");
+            println!("{}",format!("\nGame is draw!\n\n\n\n\n\n").yellow().bold());
             break;
         }
         current_player=if current_player=="X"{"O"}else{"X"};
     }
 }
 
+fn print_tab(ttt:&[[&str;3];3])
+{
+     
+    println!("\n\n");
+    println!("|-----|-----|-----|");
+    for r in 0..3
+    {
+        for c in 0..3
+        {
+            if ttt[r][c] == "X"
+            {
+                print!("|  {}  ", ttt[r][c].red());
+            }
+            else if ttt[r][c] == "O"
+            {
+                print!("|  {}  ", ttt[r][c].green());
+            }
+            else
+            {
+                print!("|  {}  ", ttt[r][c]);
+            }
+        }
+        println!("|");
 
-
+        if r < 3
+        {
+            println!("|-----|-----|-----|");
+        }
+    }
+    println!("\n\n");
+}
 
 
 
 
 fn print_pro(ttt:&[[&str;3];3],current_player:&str,p1:(usize,usize),p2:(usize,usize),p3:(usize,usize))
 {
-    println!("\n\nPlayer {} you Won the Game!",current_player);
+    if current_player=="X"
+    {
+        println!("\nPlayer {} you won the game\n\n\n\n","X".red());
+    }
+    if current_player=="O"
+    {
+        println!("\nPlayer {} you won the game\n\n\n\n","O".green());
+    }
+    println!("|----|-----|----|");
     for r in 0..3
     {
+        print!("|");
         for c in 0..3
         {
             if (r,c)==p1||(r,c)==p2||(r,c)==p3
             {
-                print!(" {} ",ttt[r][c].red());
+                if current_player=="X"
+                {
+                    print!(" {} ",ttt[r][c].red().bold());
+                }
+                if current_player=="O"
+                {
+                    print!(" {} ",ttt[r][c].green().bold());
+                }
             }
             else
             {
-                print!("{}",ttt[r][c]);
+                print!(" {} ",ttt[r][c].bright_black());
             }
             if c<2
             {
@@ -94,11 +131,12 @@ fn print_pro(ttt:&[[&str;3];3],current_player:&str,p1:(usize,usize),p2:(usize,us
             }
 
         }
-        println!();
+        println!("|");
         if r<2
         {
-            println!(" ---|---|---");
+            println!("|----|-----|----|");
         }
     }
-    return
+    println!("|----|-----|----|");
+    println!("\n\n\n\n\n\n\n\n\n\n");
 }
