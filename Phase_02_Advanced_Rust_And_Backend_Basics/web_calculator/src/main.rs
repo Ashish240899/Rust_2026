54e50139b4171aeddfe265071d205f3f6553dc84
Ashish <ashishkhattry7@gmail.com>
2026-07-18 20:32:52 +0530

    Refactor: Upgrade error handling to expect() and add Modulo/Power operations

--- Phase_02_Advanced_Rust_And_Backend_Basics/web_calculator/src/main.rs
+++ Phase_02_Advanced_Rust_And_Backend_Basics/web_calculator/src/main.rs
@@ -2,13 +2,13 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 fn main()
 {
-    let server=TcpListener::bind("127.0.0.1:8080").unwrap();
+    let server=TcpListener::bind("127.0.0.1:8080").expect("Port 8080 is Busy!");
     println!("Boom server is conneted and LIVE on https://127.0.0.1:8080");
     for connection in server.incoming()
     {
-        let mut connect=connection.unwrap();
+        let mut connect=connection.expect("Connection error!");
         let mut buffer=[0;1024];
-        connect.read(&mut buffer).unwrap();
+        connect.read(&mut buffer).expect("Error while taking request!");
         let user_info=String::from_utf8_lossy(&buffer[..]);
         if user_info.contains("GET / HTTP/1.1")
         {
@@ -21,8 +21,10 @@ fn main()
              <button type='submit' name='op' value='sub'> SUBSTRACTION </button>
              <button type='submit' name='op' value='mul'> MULTIPLICATION </button>
              <button type='submit' name='op' value='div'> DIVISION </button>
+             <button type='submit' name='op' value='rem'> REMINDER </button>
+             <button type='submit' name='op' value='pow'> POWER </button>
              </form>";
-             connect.write_all(html_form.as_bytes()).unwrap();
+             connect.write_all(html_form.as_bytes()).expect("Error while responding!");
         }
         else if user_info.contains("GET /?n1=")
         {
@@ -41,19 +43,19 @@ fn main()
                 {
             let result=format!("{}",n1+n2);
             let r=format!("HTTP/1.1 200 OK\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",result);
-            connect.write_all(r.as_bytes()).unwrap();
+            connect.write_all(r.as_bytes()).expect("Error while responding!");
                 },
                 "sub"=>
                 {
                     let result=format!("{}",n1-n2);
             let r=format!("HTTP/1.1 200 OK\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",result);
-            connect.write_all(r.as_bytes()).unwrap();
+            connect.write_all(r.as_bytes()).expect("Error while responding!");
                 },
                 "mul"=>
                 {
                     let result=format!("{}",n1*n2);
             let r=format!("HTTP/1.1 200 OK\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",result);
-            connect.write_all(r.as_bytes()).unwrap();
+            connect.write_all(r.as_bytes()).expect("Error while responding!");
                 },
                 "div"=>
                 {
@@ -66,20 +68,34 @@ fn main()
                     {
                     let result=format!("{}",n1/n2);
             let r=format!("HTTP/1.1 200 OK\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",result);
-            connect.write_all(r.as_bytes()).unwrap();
+            connect.write_all(r.as_bytes()).expect("Error while responding!");
                     }
+                }
+                "pow"=>
+                {
+                    let nn1:u32=n1 as u32;
+                    let nn2:u32=n2 as u32;
+                    let r=format!("{}",nn1.pow(nn2));
+                    let res=format!("HTTP/1.1\r\ncontent-type:text/html;charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",r);
+                    connect.write_all(res.as_bytes()).expect("Error while responding!");
+                }
+                 "rem"=>
+                {
+                    let r=format!("{}",n1%n2);
+                    let res=format!("HTTP/1.1\r\ncontent-type:text/html;charset=utf-8\r\n\r\n<h3>RESULT={}</h3><a href='/'>go back</a>",r);
+                    connect.write_all(res.as_bytes()).expect("Error while responding!");
                 }
                 _ => 
                 {
             let r = "HTTP/1.1 400 BAD REQUEST\r\nContent-Type: text/html\r\n\r\n<h2>Invalid Operator!</h2><a href='/'>Go Back</a>";
-            connect.write_all(r.as_bytes()).unwrap();
+            connect.write_all(r.as_bytes()).expect("Error while responding!");
                 }
             }
              
         }
         else
         {
-            connect.write_all("HTTP/1.1 404 NOT FOUND\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h2>page not found</h2><a href='/'>go back</a>".as_bytes()).unwrap();
+            connect.write_all("HTTP/1.1 404 NOT FOUND\r\ncontent-type:text/html; charset=utf-8\r\n\r\n<h2>page not found</h2><a href='/'>go back</a>".as_bytes()).expect("Error while responding!");
         }
     }
 
