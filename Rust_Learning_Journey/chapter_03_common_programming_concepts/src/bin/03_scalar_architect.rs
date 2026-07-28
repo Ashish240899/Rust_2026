01ec495fff1b011f1def661f282e168a5f19baf3
Ashish <ashishkhattry7@gmail.com>
2026-07-28 19:47:20 +0530

    Renamed currect program 03 name and also does some missing operation in program 03

--- Rust_Learning_Journey/chapter_03_common_programming_concepts/src/bin/03_scalar_architect.rs
+++ Rust_Learning_Journey/chapter_03_common_programming_concepts/src/bin/03_scalar_architect.rs
@@ -1,10 +1,10 @@
-//03_scaler_architect
+//03_scalar_architect
 fn main()
 {
     let age:u8=26;
     let salary:f64=25_00_000.78_000;
-    let is_active=true;
-    let grade='A';
+    let is_active:bool=true;
+    let grade:char='A';
     println!("CTO detail age={age} salary={salary} is active={is_active} and grade={grade}");
     let a:f64=10.0;
     let b:f64=20.0;
@@ -16,4 +16,6 @@ fn main()
     println!("Multiplication={c}");
     let c:f64=a/b;
     println!("Division={c}");
+    let c:f64=a%b;
+    println!("Remainder={c}");
 }
