41ddba8baa0a7cecc82cf9aad571116e6fc3d6c4
Ashish <ashishkhattry7@gmail.com>
2026-07-28 16:52:50 +0530

    Added 2nd program to chapter 03

---
+++ Rust_Learning_Journey/chapter_03_common_programming_concepts/src/bin/02_the_shadow_parser.rs
@@ -0,0 +1,10 @@
+//02_the_shadow_parser
+fn main()
+{
+    let data="    404    ";
+    println!("={data}");
+    let data=data.trim();
+    println!("={data}");
+    let data:u32=404;
+    println!("={data}");
+}
