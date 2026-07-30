09f9a28d5ccf1a4fbc03ed79a3f6924ba0304eb1
Ashish <ashishkhattry7@gmail.com>
2026-07-30 11:59:29 +0530

    Added program 9 in chapter 04

---
+++ Rust_Learning_Journey/chapter_04_understanding_ownership/src/bin/09_first_word_slicer.rs
@@ -0,0 +1,11 @@
+//09_first_word_slicer
+fn main()
+{
+    let s1:String=String::from("India is independent on 15 august 1947!");
+    let s2=first_word(&s1);
+    println!("First string is ={s1}\nSecond string is ={s2}");
+}
+fn first_string(s1:& String)->&String
+{
+    for _ in s//bhi ye 9th program kaise banega isko jara batade
+}
