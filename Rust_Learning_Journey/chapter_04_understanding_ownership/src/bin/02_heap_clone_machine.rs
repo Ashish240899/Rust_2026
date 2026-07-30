17b3964c4ddda8cbc2f25e841a876fd0027c1a10
Ashish <ashishkhattry7@gmail.com>
2026-07-30 11:09:21 +0530

    Created program 2 that it should be

--- Rust_Learning_Journey/chapter_04_understanding_ownership/src/bin/02_heap_clone_machine.rs
+++ Rust_Learning_Journey/chapter_04_understanding_ownership/src/bin/02_heap_clone_machine.rs
@@ -1,7 +1,7 @@
 //02_heap_clone_machine
 fn main()
 {
-    let s1=String::from("Car 27XPT9002");
-    let s2=s1.clone();
-    println!("Original: {s1}, Clone: {s2}");
+    let original_data=String::from("This is original data!");
+    let clone_data=original_data.clone();
+    println!("Original data={original_data} and clone data={clone_data}");
 }
