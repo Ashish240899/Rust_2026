38352d4e27697bd989ab8f24d384d5d015703241
Ashish <ashishkhattry7@gmail.com>
2026-08-01 14:06:51 +0530

    Added program 05 to chapter 06

---
+++ rust_learning_journey/chapter_06_enums_and_pattern_matching/src/bin/05_coin_sorter_match.rs
@@ -0,0 +1,29 @@
+//05_coin_sorter_match
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Coin
+{
+    Penny,
+    Nickel,
+    Dime,
+    Quater,
+}
+impl Coin
+{
+    fn value_in_cents(&self)->u32
+    {
+        match self
+        {
+            Coin::Penny=>1,
+            Coin::Nickel=>5,
+            Coin::Dime=>10,
+            Coin::Quater=>25,
+        }
+    }
+}
+fn main()
+{
+    let c=Coin::Penny;
+    let value=c.value_in_cents();
+    println!("Coin value={:?}",value);
+}
