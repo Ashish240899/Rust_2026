d9f1e363516f151537d6018cf4fae0fb0e79c19d
Ashish <ashishkhattry7@gmail.com>
2026-08-01 14:21:05 +0530

    Added program 07 to chapter 06

---
+++ rust_learning_journey/chapter_06_enums_and_pattern_matching/src/bin/07_option_enum_match.rs
@@ -0,0 +1,22 @@
+//07_option_enum_match
+#[allow(dead_code)]
+fn plus_one(x:Option<i32>)->Option<i32>
+{
+    match x //this code has erro fix it later
+    {
+        Some(n)=>Some(n+1),
+        None=>
+        {
+            println!("This is none");
+            return None;
+        }
+    }
+}
+fn main()
+{
+    let a:Option<i32>=Some(32);
+    let b=None;
+    let i=plus_one(a);
+    println!("I={:?}",i);
+
+}
