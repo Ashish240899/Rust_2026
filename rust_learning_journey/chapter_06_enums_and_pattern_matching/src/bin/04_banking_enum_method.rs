5e1df5997d31dced6b870961fac4785a3ed2f89a
Ashish <ashishkhattry7@gmail.com>
2026-08-01 15:11:29 +0530

    Currected a spelling mistake trasaction to transaction in program 04

--- rust_learning_journey/chapter_06_enums_and_pattern_matching/src/bin/04_banking_enum_method.rs
+++ rust_learning_journey/chapter_06_enums_and_pattern_matching/src/bin/04_banking_enum_method.rs
@@ -1,12 +1,12 @@
 //04_banking_enum_method
 #[derive(Debug)]
 #[allow(dead_code)]
-enum Trasaction
+enum Transaction
 {
     Deposit(u32),
     Withdraw(u32),
 }
-impl Trasaction
+impl Transaction
 {
     fn process(&self)
     {
@@ -15,8 +15,8 @@ impl Trasaction
 }
 fn main()
 {
-    let d=Trasaction::Deposit(5_00_000);
-    let w=Trasaction::Withdraw(25_000);
+    let d=Transaction::Deposit(5_00_000);
+    let w=Transaction::Withdraw(25_000);
     d.process();
     w.process();
     println!("Deposit ={:?} withdraw={:?}",d,w);
