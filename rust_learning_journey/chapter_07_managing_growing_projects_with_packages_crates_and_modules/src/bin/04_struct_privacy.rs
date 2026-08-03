a780c55ab77acb061d8d15ab9177ad3961b412db
Ashish <ashishkhattry7@gmail.com>
2026-08-03 16:59:16 +0530

    Added program 04 to chapter 06

---
+++ rust_learning_journey/chapter_07_managing_growing_projects_with_packages_crates_and_modules/src/bin/04_struct_privacy.rs
@@ -0,0 +1,26 @@
+//04_struct_privacy
+mod ecommerce
+{
+    pub struct UserAccount
+    {
+        pub username:String,
+        credit_card:String,
+    }
+    impl UserAccount
+    {
+        pub fn new()->Self
+        {
+            Self
+            {
+                username:String::new(),
+                credit_card:String::new(),
+            }
+        }
+    }
+}
+fn main()
+{
+    let mut u=ecommerce::UserAccount::new();
+    u.username=String::from("Ashish");
+    println!("={:?}",u.username);
+}
